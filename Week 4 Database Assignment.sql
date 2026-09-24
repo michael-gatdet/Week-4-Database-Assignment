5cf5033ce8868bfc67c90b80de5f5bcb6043ca9c
Bhan-gatdet <bhangatdet00@gmail.com>
2026-09-24 12:44:08 +0300

    complete week 4 database assignment

---
+++ Week 4 Database Assignment.sql
@@ -0,0 +1,28 @@
+USE sales;
+-- Question 1
+SELECT paymentDate, SUM(amount) AS total_amount
+FROM payments
+GROUP BY paymentDate
+ORDER BY paymentDate DESC
+LIMIT 5;
+
+
+-- Question 2
+SELECT customerName, country, AVG(creditLimit) AS average_credit_limit
+FROM customers
+GROUP BY customerName, country;
+
+
+-- Question 3
+SELECT 
+    productCode,
+    quantityOrdered,
+    SUM(quantityOrdered * priceEach) AS total_price
+FROM orderdetails
+GROUP BY productCode, quantityOrdered;
+
+
+-- Question 4
+SELECT checkNumber, MAX(amount) AS highest_amount
+FROM payments
+GROUP BY checkNumber;
